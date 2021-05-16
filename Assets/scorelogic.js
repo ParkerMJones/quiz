a708ab28f79df3cd466a332fe35617cacf071677
Parker Jones <parker.matthewjones@gmail.com>
2021-05-15 18:51:30 -0700

    Only One Winner

--- Assets/scorelogic.js
+++ Assets/scorelogic.js
@@ -1,15 +1,19 @@
 var scoresList = document.getElementById('scoresList');
 var scoreItem = document.getElementById("scoreItem");
 var clearHighScores = document.getElementById("clearHighScores");
+var numberOfScores = 10;
+var gameResultArray = [];
 
 
 var scoreboard = function() {
 var secondsLeft = localStorage.getItem("secondsLeft");
 var getName = localStorage.getItem("getName");
-document.getElementById("scoreItem").textContent = getName + " : " + secondsLeft;
+scoreItem.textContent = getName + " : " + secondsLeft;
+
 };
 
 clearHighScores.addEventListener("click", function() {
     localStorage.clear();
 })
+
 scoreboard();
